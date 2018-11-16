08547a0ea30300b3f5608b82e634d084876ecc88
micah-jaffe <micah.jaffe95@gmail.com>
2018-11-16 14:32:18 -0800

    Write tests and implement User controller

---
+++ goal-app/app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+  def index
+    render :index
+  end
+  
+  def show
+    render :show
+  end
+  
+  def new
+    render :new
+  end
+  
+  def edit
+    render :edit
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_url(@user)
+    else
+      flash[:errors] = "Invalid credentials"
+      render :new
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update_attributes(user_params)
+      redirect_to user_url(@user)
+    else
+      flash[:errors] = "Invalid credentials"
+      render :edit
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy!
+    redirect_to new_user_url
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
---
+++ goal-app/spec/controllers/users_controller_spec.rb
@@ -0,0 +1,96 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  
+  describe 'GET #index' do
+    it 'renders the /users page' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+  
+  describe 'GET #show' do
+    it 'renders an individual user page' do
+      get :show, params: { id: 1 }
+      expect(response).to render_template(:show)
+    end
+  end
+  
+  describe 'GET #new' do
+    it 'renders the new user page' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+  
+  describe 'GET #edit' do
+    it 'renders the edit user page' do
+      get :edit, params: { id: 1 }
+      expect(response).to render_template(:edit)
+    end
+  end
+  
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new user' do
+        post :create, params: { user: { username: 'new_user', password: 'password' } }
+        user = User.find_by(username: 'new_user')
+        
+        expect(user).not_to be_nil
+        expect(response).to redirect_to(user_url(user))
+      end
+    end
+    
+    context 'with invalid params' do
+      it 'does not create user renders new' do
+        post :create, params: { user: { username: '', password: 'password' } }
+        user = User.find_by(username: 'new_user')
+        
+        expect(user).to be_nil
+        expect(response).to render_template(:new)
+        expect(flash[:errors]).to be_present
+      end
+    end
+  end
+    
+  describe 'PATCH #update' do
+    
+    before(:each) do
+      let(:user) { User.create!(username: 'username', password: 'password') }
+    end
+
+    context 'with valid params' do
+      it 'updates user' do
+        # user = User.create!(username: 'username', password: 'password')
+        patch :update, params: { id: user.id, user: { username: 'updated_username', password: 'password' } }
+        user = User.find_by(username: 'updated_username')
+        
+        expect(user).not_to be_nil
+        expect(response).to redirect_to(user_url(user))
+      end
+    end
+    
+    context 'with invalid params' do
+      it 'does not update user and renders the edit page again' do
+        # user = User.create!(username: 'username', password: 'password')
+        patch :update, params: { id: user.id, user: { username: '', password: 'password' } }
+        user = User.find_by(username: '')
+        
+        expect(user).to be_nil
+        expect(response).to render_template(:edit)
+        expect(flash[:errors]).to be_present
+      end
+    end
+  end
+  
+  describe 'DELETE #destroy' do
+    it 'removes user from database' do
+      # user = User.create!(username: 'username', password: 'password')
+      delete :destroy, params: { id: user.id }
+      
+      expect { User.find(user.id) }.to raise_error
+      expect(response).to redirect_to(new_user_url)
+    end
+  end
+  
+end
